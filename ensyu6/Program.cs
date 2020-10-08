e2fcb865f92fe8a0919b85d6d54c29342ec53ef5
TomoyaIizuka <30188@ojs.ac.jp>
2020-10-08 11:57:57 +0900

    6章演習問題　開始

---
+++ ensyu6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensyu6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            #region 6.1
+            Console.WriteLine("問6.1");
+            var max = numbers.Max();
+            Console.WriteLine(max);
+            #endregion
+
+            #region 6.2
+            Console.WriteLine("\n--------------------");
+            Console.WriteLine("問6.2");
+            //var 
+            #endregion
+
+            #region 6.3
+            Console.WriteLine("\n--------------------");
+            Console.WriteLine("問6.3");
+            
+            #endregion
+
+
+        }
+    }
+}
